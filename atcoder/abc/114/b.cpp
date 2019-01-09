1dd9b19b60ea32a38aba8c7a59234213cbeeb925
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-01-09 14:42:18 +0900

    abc 114

---
+++ atcoder/abc/114/b.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+
+	int minDiff = INT_MAX;
+	for (int i = 0; (size_t) i < s.size() - 2; i++) {
+		int ss = atoi(s.substr(i, 3).c_str());
+		int diff = abs(ss - 753);
+		if (diff < minDiff) {
+			minDiff = diff;
+		}
+	}
+
+	cout << minDiff << endl;
+	return 0;
+}
