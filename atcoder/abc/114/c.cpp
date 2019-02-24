9e66691698bec1c2ffc3865f4c76402a72cb3acf
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-02-24 21:24:22 +0900

    abc 114 c

---
+++ atcoder/abc/114/c.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int ans = 0;
+int n;
+
+bool is357(long long n)
+{
+	bool three, five, seven;
+	three = five = seven = false;
+
+	string s = to_string(n);
+	for (int i = 0; (size_t) i < s.size(); i++) {
+		if (s[i] == '3') {
+			three = true;
+		} else if (s[i] == '5') {
+			five = true;
+		} else if (s[i] == '7') {
+			seven = true;
+		}
+	}
+
+	return three && five && seven;
+}
+
+void search(long long head)
+{
+	if (head <= n) {
+		if (is357(head)) {
+			ans++;
+		}
+		search(head * 10 + 3);
+		search(head * 10 + 5);
+		search(head * 10 + 7);
+	}
+}
+
+int main()
+{
+	cin >> n;
+
+	search(0);
+
+	cout << ans << endl;
+	return 0;
+}
