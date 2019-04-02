bfdfc172d26dc866b10224330a59f2d7ab86b835
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-04-02 23:13:47 +0900

    abc 121 d

---
+++ atcoder/abc/121/d.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long axor(long long a)
+{
+	if (a % 4 == 0)
+		return a;
+	if (a % 4 == 1)
+		return 1;
+	if (a % 4 == 2)
+		return a + 1;
+	if (a % 4 == 3)
+		return 0;
+
+	// g++ -Wall warning
+	return 0;
+}
+
+int main()
+{
+	long long A, B;
+	cin >> A >> B;
+
+	cout << (axor(B) ^ axor(A - 1)) << endl;
+
+	return 0;
+}
