458620581ee9c0e7a9cdeb16cddaf1f7d7908393
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-04-04 11:18:09 +0900

    abc 086 b

---
+++ atcoder/abc/086/b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string a, b;
+	cin >> a >> b;
+
+	int n = atoi((a + b).c_str());
+
+	int i = 1;
+	while (i * i < n) {
+		i++;
+	}
+	if (i * i == n) {
+		cout << "Yes" << endl;
+	} else {
+		cout << "No" << endl;
+	}
+	return 0;
+}
