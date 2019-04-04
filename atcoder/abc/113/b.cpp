d8e8885f522ace75e31be9cddcd33935e44b58d9
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-04-04 23:34:02 +0900

    abc 113 ab

---
+++ atcoder/abc/113/b.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+double T, A;
+
+double tempDiff(double h)
+{
+	return abs(A - (T - h * 0.006));
+}
+
+int main()
+{
+	double N;
+	cin >> N;
+	cin >> T >> A;
+	vector < double >H(N);
+	for (int i = 0; i < N; i++) {
+		cin >> H[i];
+	}
+
+	int ans = 0;
+	for (int i = 1; i < N; i++) {
+		if (tempDiff(H[i]) < tempDiff(H[ans])) {
+			ans = i;
+		}
+	}
+
+	cout << ans + 1 << endl;
+
+	return 0;
+}
