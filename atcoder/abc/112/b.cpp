93f51d2c1d1c254e7f486860f35c7885e105bbd1
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-04-13 20:56:06 +0900

    abc 112 a,b

---
+++ atcoder/abc/112/b.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N, T;
+	cin >> N >> T;
+
+	vector < pair < int, int >>v;
+	for (int i = 0; i < N; i++) {
+		int c, t;
+		cin >> c >> t;
+		v.push_back(make_pair(c, t));
+	}
+
+	sort(v.begin(), v.end());
+
+ for (auto vc:v) {
+		if (vc.second <= T) {
+			cout << vc.first << endl;
+			return 0;
+		}
+	}
+
+	cout << "TLE" << endl;
+
+	return 0;
+}
