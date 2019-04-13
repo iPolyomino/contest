58c02571cc73a96a272fb81c6e06d6c0d945bdd7
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-04-14 01:33:05 +0900

    arc 001 a

---
+++ atcoder/arc/001/a.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	string S;
+	cin >> S;
+
+	int maxCount = INT_MIN;
+	int minCount = INT_MAX;
+	for (int i = 0; i < 4; i++) {
+		int cnt = count(S.begin(), S.end(), (char)('1' + i));
+		maxCount = max(maxCount, cnt);
+		minCount = min(minCount, cnt);
+	}
+
+	cout << maxCount << " " << minCount << endl;
+
+	return 0;
+}
