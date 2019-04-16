502a1d8daef33fadbae28475291c49547ab08bdf
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-04-16 13:47:31 +0900

    abc 111

---
+++ atcoder/abc/111/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	for (int i = 0; size_t(i) < s.size(); i++) {
+		s[i] = '9' + '1' - s[i];
+	}
+	cout << s << endl;
+	return 0;
+}
---
+++ atcoder/abc/111/b.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX 999
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	for (int i = N; i <= MAX; i++) {
+		if (i % 111 == 0) {
+			cout << i << endl;
+			break;
+		}
+	}
+
+	return 0;
+}
