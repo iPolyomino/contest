07034b0b697d8d90aee81d1201463b033b87d935
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-04-23 11:54:23 +0900

    abc 109 c

---
+++ atcoder/abc/109/c.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector < int >x(N + 1);
+	for (int i = 0; i < N + 1; i++) {
+		cin >> x[i];
+	}
+
+	sort(x.begin(), x.end());
+
+	int diffGCD = x[1] - x[0];
+	for (int i = 1; i < N; i++) {
+		diffGCD = __gcd(diffGCD, x[i + 1] - x[i]);
+	}
+
+	cout << diffGCD << endl;
+
+	return 0;
+}
