88314a9ae82776c765401c249f83f8a2f5cb4a2a
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-05-19 16:15:19 +0900

    abc 104 ab

---
+++ atcoder/abc/104/b.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string S;
+	cin >> S;
+
+	bool startA = S[0] == 'A';
+	bool containsC = false;
+	bool allSmall = true;
+
+	for (int i = 1; (size_t) i < S.size(); i++) {
+		if (S[i] < 'a' || 'z' < S[i]) {
+			if (i != 1 && !containsC && S[i] == 'C'
+			    && i != int (S.size() - 1)) {
+				containsC = true;
+				continue;
+			}
+			allSmall = false;
+		}
+	}
+
+	if (startA && containsC && allSmall) {
+		cout << "AC" << endl;
+	} else {
+		cout << "WA" << endl;
+	}
+	return 0;
+}
