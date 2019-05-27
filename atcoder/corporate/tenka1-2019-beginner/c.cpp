c4cff1b36a615dda34fd229345a9bdb9e269cd5d
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-05-27 18:12:41 +0900

    tenka1-2019-beginner abc

---
+++ atcoder/corporate/tenka1-2019-beginner/c.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	string S;
+	cin >> S;
+
+	int white = 0, black = 0;
+
+	for (int i = 0; i < N; i++) {
+		if (S[i] == '.') {
+			white++;
+		}
+	}
+
+	int ans = white;
+	for (int i = 0; i < N; i++) {
+		if (S[i] == '.') {
+			white--;
+		}
+		if (S[i] == '#') {
+			black++;
+		}
+		ans = min(ans, white + black);
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
