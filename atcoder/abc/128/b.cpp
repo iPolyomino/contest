15b9b78447cf9a885a2bf57f59b5edaee5e9c5c7
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-06-12 23:30:49 +0900

    abc 128 ab

---
+++ atcoder/abc/128/b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector < pair < pair < string, int >, int >>sp(N);
+	for (int i = 0; i < N; i++) {
+		string s;
+		int p;
+		cin >> s >> p;
+		sp[i] = make_pair(make_pair(s, -p), i);
+	}
+	sort(sp.begin(), sp.end());
+	for (int i = 0; i < N; i++) {
+		cout << sp[i].second + 1 << endl;
+	}
+
+	return 0;
+}
