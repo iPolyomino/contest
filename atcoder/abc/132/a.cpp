5088d95c3a789e79433e2dfb9bf6cf1c4c6e0c62
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-12-05 01:37:15 +0900

    abc 131,132,133 a

---
+++ atcoder/abc/132/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+
+	for (int i = 0; (size_t)i < s.size(); i++) {
+		if (count(s.begin(), s.end(), s[i]) != 2) {
+			cout << "No" << endl;
+			return 0;
+		}
+	}
+
+	cout << "Yes" << endl;
+	return 0;
+}
