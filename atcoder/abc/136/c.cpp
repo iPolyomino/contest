3d8c9f4fe5451c693439e45c4ed6979b44e0d5c7
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-12-08 00:35:05 +0900

    abc 126 abc

---
+++ atcoder/abc/136/c.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	vector < int >H(N);
+	for (int i = 0; i < N; i++) {
+		cin >> H[i];
+	}
+
+	int current = H[0] - 1;
+	for (int i = 0; i < N; i++) {
+		if (H[i] < current) {
+			cout << "No" << endl;
+			return 0;
+		}
+		current = H[i] == current ? current : H[i] - 1;
+	}
+
+	cout << "Yes" << endl;
+
+	return 0;
+}
