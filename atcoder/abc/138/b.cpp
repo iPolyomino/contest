0d13657748bc01791c31833f273023e3df75e202
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-12-08 01:09:20 +0900

    abc 137,138 ab

---
+++ atcoder/abc/138/b.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	vector < int >A(N);
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
+
+	double ans = 0;
+	for (int i = 0; i < N; i++) {
+		ans += 1.0 / (double)A[i];
+	}
+
+	printf("%.10f\n", 1.0 / ans);
+	return 0;
+}
