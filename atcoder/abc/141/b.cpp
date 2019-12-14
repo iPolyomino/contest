7ba6f9466a66d5c7fc5991e363c1ffb2ee54f416
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-12-14 15:16:45 +0900

    abc 141 ab

---
+++ atcoder/abc/141/b.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string S;
+	cin >> S;
+
+	bool easy = true;
+
+	for (int i = 0; (size_t)i < S.size(); i++) {
+		if ((i % 2 == 0 && S[i] == 'L') || (i % 2 == 1 && S[i] == 'R')) {
+			easy = false;
+		}
+	}
+
+	if (easy) {
+		cout << "Yes" << endl;
+	} else {
+		cout << "No" << endl;
+	}
+
+	return 0;
+}
