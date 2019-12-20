ce29031adbd218e981e0240ec6e15c46d2428d06
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-12-21 01:37:23 +0900

    abc 143 abc

---
+++ atcoder/abc/143/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int A, B;
+	cin >> A >> B;
+
+	int ans = A - B * 2;
+	if (ans < 0) {
+		cout << 0 << endl;
+	} else {
+		cout << ans << endl;
+	}
+	return 0;
+}
---
+++ atcoder/abc/143/c.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	string S;
+	cin >> S;
+
+	int ans = 1;
+	char current = S[0];
+	for (int i = 1; i < N; i++) {
+		if (current != S[i]) {
+			ans++;
+			current = S[i];
+		}
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
