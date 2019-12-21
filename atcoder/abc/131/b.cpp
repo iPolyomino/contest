b6286d508dc251c5a0aefb20895959e75f90c003
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-12-21 11:56:41 +0900

    abc 131 b

---
+++ atcoder/abc/131/b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N, L;
+	cin >> N >> L;
+
+	int min = L;
+	int sum = 0;
+	for (int i = L; i <= L + N - 1; i++) {
+		if (abs(i) < abs(min)) {
+			min = i;
+		}
+		sum += i;
+	}
+
+	cout << sum - min << endl;
+
+	return 0;
+}
