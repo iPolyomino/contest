cb111b3cdac329bbbf61d324cc085821c7087519
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-12-29 02:16:33 +0900

    abc 135 b

---
+++ atcoder/abc/135/b.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	vector < int >p(N);
+	for (int i = 0; i < N; i++) {
+		cin >> p[i];
+	}
+
+	int wrongIndex = -1;
+	for (int i = 0; i < N; i++) {
+		if (p[i] != i + 1) {
+			if (wrongIndex == -1) {
+				wrongIndex = i;
+			} else {
+				if (p[wrongIndex] != i + 1) {
+					cout << "NO" << endl;
+					return 0;
+				}
+			}
+		}
+	}
+	cout << "YES" << endl;
+	return 0;
+}
