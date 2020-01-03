a550292e4134e255e776ffcc0552bf1353d0f8ee
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-01-03 22:07:21 +0900

    abc 149 bc

---
+++ atcoder/abc/149/b.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	ll A, B, K;
+	cin >> A >> B >> K;
+
+	A -= K;
+	if (A < 0) {
+		B += A;
+	}
+
+	cout << max((ll) 0, A) << " " << max((ll) 0, B) << endl;
+
+	return 0;
+}
---
+++ atcoder/abc/149/c.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector < int >prime_number;
+
+bool isPrime(int n)
+{
+ for (auto p:prime_number) {
+		if (n % p == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	int x;
+	cin >> x;
+
+	int i = 2;
+	while (i < x || !isPrime(i)) {
+		if (isPrime(i)) {
+			prime_number.push_back(i);
+		}
+		i++;
+	}
+
+	cout << i << endl;
+
+	return 0;
+}
