094a0904c3161b3332a69a37a32f12a40bd66bbd
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-01-06 01:06:06 +0900

    abc 149 d

---
+++ atcoder/abc/149/d.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N, K;
+	cin >> N >> K;
+	int R, S, P;
+	cin >> R >> S >> P;
+	string T;
+	cin >> T;
+
+	int score = 0;
+	for (int i = 0; i < N; i++) {
+		if (K <= i && T[i] == T[i - K]) {
+			T[i] = '0';
+			continue;
+		}
+
+		if (T[i] == 'r') {
+			score += P;
+		}
+		if (T[i] == 's') {
+			score += R;
+		}
+		if (T[i] == 'p') {
+			score += S;
+		}
+	}
+
+	cout << score << endl;
+	return 0;
+}
