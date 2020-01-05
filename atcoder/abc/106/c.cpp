0a9bfe2573ac7627034c25c0d1f2a879eb3f3697
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-01-05 13:13:11 +0900

    abc 106 c

---
+++ atcoder/abc/106/c.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	string S;
+	cin >> S;
+
+	ll K;
+	cin >> K;
+
+	for (int i = 0; i < min(K, (ll) S.size()); i++) {
+		if (S[i] != '1') {
+			cout << S[i] << endl;
+			return 0;
+		}
+	}
+
+	cout << 1 << endl;
+	return 0;
+}
