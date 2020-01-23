6d5099f6dba40d2e6bfb3cd6ecc393c5c61ab164
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-01-24 01:36:44 +0900

    atc 002 b

---
+++ atcoder/atc/002/b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	ll N, M, P;
+	cin >> N >> M >> P;
+
+	ll n = N;
+	ll ans = 1;
+	for (ll k = P; k > 0; k >>= 1) {
+		if (k & 1) {
+			ans = (ans * n) % M;
+		}
+		n = (n * n) % M;
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
