35bbbe1e1d83f7b5d9588b4ead40506fa1c7b4b3
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-01-25 19:27:10 +0900

    abc 142 cd

---
+++ atcoder/abc/142/c.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	vector < pair < int, int >>A(N);
+
+	for (int i = 0; i < N; i++) {
+		int a;
+		cin >> a;
+		A[i] = make_pair(a, i + 1);
+	}
+
+	sort(A.begin(), A.end());
+
+	for (int i = 0; i < N; i++) {
+		cout << A[i].second << " ";
+	}
+	cout << "\n";
+	return 0;
+}
---
+++ atcoder/abc/142/d.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	ll A, B;
+	cin >> A >> B;
+	ll gcd = __gcd(A, B);
+
+	ll ans = 0;
+	for (ll i = 2; i * i <= gcd; i++) {
+		if (gcd % i == 0) {
+			ans++;
+			while (gcd % i == 0) {
+				gcd /= i;
+			}
+		}
+	}
+
+	if (gcd != 1) {
+		ans++;
+	}
+
+	cout << ans + 1 << endl;
+
+	return 0;
+}
