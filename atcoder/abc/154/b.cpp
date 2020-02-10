30d25b32ea6e526c16f80e717bf7089b7be15dd8
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-02-10 23:10:18 +0900

    abc 154 abcd

---
+++ atcoder/abc/154/b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string S;
+	cin >> S;
+	for (int i = 0; (size_t)i < S.size(); i++) {
+		cout << "x";
+	}
+	cout << "\n";
+	return 0;
+}
---
+++ atcoder/abc/154/d.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N, K;
+	cin >> N >> K;
+	vector < int >p(N);
+	for (int i = 0; i < N; i++) {
+		cin >> p[i];
+	}
+
+	vector < double >pp(N);
+	double sum = 0;
+	for (int i = 0; i < K; i++) {
+		pp[i] = (double)(p[i] + 1) / 2;
+		sum += pp[i];
+	}
+
+	double maxSum = sum;
+
+	for (int i = K; i < N; i++) {
+		pp[i] = (double)(p[i] + 1) / 2;
+
+		sum -= pp[i - K];
+		sum += pp[i];
+		maxSum = max(maxSum, sum);
+	}
+
+	printf("%.10f\n", maxSum);
+
+	return 0;
+}
