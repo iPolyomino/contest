b1929043b84b2ff95fbb6952b44b0b7712937f69
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-03-17 22:28:06 +0900

    abc 144 d

---
+++ atcoder/abc/144/d.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	double a, b, x;
+	cin >> a >> b >> x;
+	double full = a * a * b;
+	double half = full / 2.0;
+	if (half <= x) {
+		double height = (full - x) / (a * a) * 2;
+		double theta = atan(height / a) * 180 / M_PI;
+		printf("%.10f\n", theta);
+	} else {
+		double height = x / (a * b) * 2;
+		double theta = (M_PI / 2 - atan(height / b)) * 180 / M_PI;
+		printf("%.10f\n", theta);
+	}
+
+	return 0;
+}
