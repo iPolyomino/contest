6d90881fc53f889bcb7c26d185322f5c732d3e03
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-04-16 15:01:53 +0900

    abc 102 ab

---
+++ atcoder/abc/102/b.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	int N;
+	cin >> N;
+	vector < ll > A(N);
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
+
+	sort(A.begin(), A.end());
+
+	cout << A[N - 1] - A[0] << endl;
+
+	return 0;
+}
