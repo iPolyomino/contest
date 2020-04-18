21ee6361c81d39e2d42ebecfd387251346dd1e6a
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-04-18 13:55:43 +0900

    abc 100 ab

---
+++ atcoder/abc/100/b.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int div(int n)
+{
+	int cnt = 0;
+	while (n % 100 == 0) {
+		n /= 100;
+		cnt++;
+	}
+	return cnt;
+}
+
+int main()
+{
+	int D, N;
+	cin >> D >> N;
+
+	int nb = 0;
+	int i = 0;
+	while (nb < N) {
+		i++;
+		if (div(i) == D) {
+			nb++;
+		}
+	}
+
+	cout << i << endl;
+
+	return 0;
+}
