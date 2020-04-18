94dbd7fdbeb232ed8a119bb4b0477dd02e285af2
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-04-18 20:21:00 +0900

    abc 100 c

---
+++ atcoder/abc/100/c.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	ll N;
+	cin >> N;
+	vector < ll > a(N);
+	for (int i = 0; i < N; i++) {
+		cin >> a[i];
+	}
+
+	ll ans = 0;
+	for (int i = 0; i < N; i++) {
+		while (a[i] % 2 == 0) {
+			a[i] /= 2;
+			ans++;
+		}
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
