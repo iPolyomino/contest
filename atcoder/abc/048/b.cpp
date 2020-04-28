b192811a11ccc8f11b8f0d5648e5f43febf421a5
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-04-28 12:46:31 +0900

    abc 048 b

---
+++ atcoder/abc/048/b.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	ll a, b, x;
+	cin >> a >> b >> x;
+
+	ll ans = b / x - a / x;
+
+	if (a % x == 0) {
+		ans++;
+	}
+
+	cout << ans << endl;
+	return 0;
+}
