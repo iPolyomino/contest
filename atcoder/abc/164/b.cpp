b2a82cbb0cb9a6a95324cb7b33937c99fc302da8
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-04-28 12:36:49 +0900

    abc 164 abc

---
+++ atcoder/abc/164/b.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	double A, B, C, D;
+	cin >> A >> B >> C >> D;
+	if (ceil(A / D) >= ceil(C / B)) {
+		cout << "Yes" << endl;
+	} else {
+		cout << "No" << endl;
+	}
+	return 0;
+}
---
+++ atcoder/abc/164/c.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	vector < string > S(N);
+	for (int i = 0; i < N; i++) {
+		cin >> S[i];
+	}
+
+	sort(S.begin(), S.end());
+	S.erase(unique(S.begin(), S.end()), S.end());
+
+	cout << S.size() << endl;
+
+	return 0;
+}
