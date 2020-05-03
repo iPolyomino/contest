db3195b7d039aa5d05c2b156b06d1e75dba69a2f
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-05-03 13:05:21 +0900

    abc 165 cd

---
+++ atcoder/abc/165/d.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	ll A, B, N;
+	cin >> A >> B >> N;
+
+	ll x = min(B - 1, N);
+
+	cout << A * x / B << endl;
+
+	return 0;
+}
