f267877c17e61cb77119236cf72df3d3dee64487
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-05-03 23:39:21 +0900

    abc 166 abc

---
+++ atcoder/abc/166/b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N, K;
+	cin >> N >> K;
+
+	vector < int >d(K);
+	vector < vector < int >>A(K);
+	for (int i = 0; i < K; i++) {
+		cin >> d[i];
+		vector < int >tmp(d[i]);
+		for (int j = 0; j < d[i]; j++) {
+			cin >> tmp[j];
+		}
+		A[i] = tmp;
+	}
+
+	set < int >st;
+	for (int i = 1; i <= N; i++) {
+		st.insert(i);
+	}
+
+	set < int >al;
+	for (int i = 0; i < K; i++) {
+		set < int >confectionery(A[i].begin(), A[i].end());
+		set_union(al.begin(), al.end(),
+			  confectionery.begin(), confectionery.end(),
+			  inserter(al, al.end()));
+	}
+
+	set < int >ans;
+	set_difference(st.begin(), st.end(),
+		       al.begin(), al.end(), inserter(ans, ans.end())
+	    );
+
+	cout << ans.size() << endl;
+
+	return 0;
+}
---
+++ atcoder/abc/166/c.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	ll N, M;
+	cin >> N >> M;
+	vector < int >H(N);
+	for (int i = 0; i < N; i++) {
+		cin >> H[i];
+	}
+	vector < int >A(M), B(M);
+	for (int i = 0; i < M; i++) {
+		cin >> A[i] >> B[i];
+		A[i]--;
+		B[i]--;
+	}
+
+	vector < set < int >>connect(N);
+	for (int i = 0; i < M; i++) {
+		connect[A[i]].insert(B[i]);
+		connect[B[i]].insert(A[i]);
+	}
+
+	int ans = 0;
+	for (int i = 0; i < N; i++) {
+		int highest = 0;
+ for (auto c:	connect[i]) {
+			highest = max(highest, H[c]);
+		}
+		if (highest < H[i]) {
+			ans++;
+		}
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
