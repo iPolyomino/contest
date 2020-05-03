adf4d5a20092755bb698413c7e82acd2440a8c49
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-05-03 15:01:44 +0900

    challenge-book best-cow-line

---
+++ challenge-book/chapter2/best-cow-line/best-cow-line.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+string S;
+
+int main()
+{
+	cin >> N;
+	cin >> S;
+
+	int a = 0, b = N - 1;
+
+	while (a <= b) {
+		bool left = false;
+		for (int i = 0; a + i <= b; i++) {
+			if (S[a + i] < S[b - i]) {
+				left = true;
+				break;
+			} else if (S[a + i] > S[b - i]) {
+				left = false;
+				break;
+			}
+		}
+		if (left) {
+			cout << S[a++];
+		} else {
+			cout << S[b--];
+		}
+	}
+
+	cout << "\n";
+
+	return 0;
+}
