33a738dc3013441df454c4dc610281077b13eec3
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-06-22 12:39:46 +0900

    abc 171 abc

---
+++ atcoder/abc/171/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	char c;
+	cin >> c;
+
+	if (c <= 'Z') {
+		cout << "A" << endl;
+	} else {
+		cout << "a" << endl;
+	}
+	return 0;
+}
---
+++ atcoder/abc/171/c.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	ll N;
+	cin >> N;
+
+	string ans = "";
+
+	while (N > 0) {
+		N--;
+		ans.insert(ans.begin(), 'a' + N % 26);
+		N /= 26;
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
