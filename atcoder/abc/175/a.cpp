477a91059d8f87c067f946bfcaadee0b3f6125a6
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-08-20 12:31:53 +0900

    abc 175 ab

---
+++ atcoder/abc/175/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string S;
+	cin >> S;
+
+	int ans = 0;
+	for (size_t i = 1; i <= S.size(); i++) {
+		if (S.find(string(i, 'R')) != string::npos) {
+			ans = i;
+		}
+	}
+
+	cout << ans << endl;
+	return 0;
+}
---
+++ atcoder/abc/175/b.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	int N;
+	cin >> N;
+	vector < ll > L(N);
+	for (int i = 0; i < N; i++) {
+		cin >> L[i];
+	}
+
+	sort(L.begin(), L.end());
+
+	int ans = 0;
+	for (int i = 0; i < N - 2; i++) {
+		for (int j = i + 1; j < N - 1; j++) {
+			for (int k = j + 1; k < N; k++) {
+				if (L[i] + L[j] > L[k] && L[i] != L[j]
+				    && L[j] != L[k]) {
+					ans++;
+				}
+			}
+		}
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
