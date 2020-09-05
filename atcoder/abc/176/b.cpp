dbff17844419a2eddab8da8c72248be4394d1f33
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-09-05 23:17:51 +0900

    abc 176 bc

---
+++ atcoder/abc/176/b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string N;
+	cin >> N;
+
+	int sum = 0;
+	for (size_t i = 0; i < N.size(); i++) {
+		sum += N[i] - '0';
+		sum %= 9;
+	}
+
+	if (sum == 0) {
+		cout << "Yes" << endl;
+	} else {
+		cout << "No" << endl;
+	}
+
+	return 0;
+}
