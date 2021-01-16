da10e21a5d291fea40d9df3fb2e4e8820e4fbd64
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-01-16 21:46:54 +0900

    abc 183 abc

---
+++ atcoder/abc/183/b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	double sx, sy, gx, gy;
+	cin >> sx >> sy >> gx >> gy;
+
+	double xdiff = gx - sx;
+	double ydiff = gy + sy;
+
+	double ans = sx + xdiff * sy / ydiff;
+
+	printf("%.10f\n", ans);
+
+	return 0;
+}
---
+++ atcoder/abc/183/c.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	ll N, K;
+	cin >> N >> K;
+
+	vector < vector < ll >> T(N, vector < ll > (N));
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> T[i][j];
+		}
+	}
+
+	vector < ll > num(N);
+	for (int i = 0; i < N; i++) {
+		num[i] = i;
+	}
+
+	ll ans = 0;
+
+	do {
+		ll sum = 0;
+
+		if (num[0] != 0) {
+			continue;
+		}
+
+		for (int i = 0; i < N; i++) {
+			sum += T[num[i]][num[(i + 1) % N]];
+		}
+		if (sum == K) {
+			ans++;
+		}
+
+	} while (next_permutation(num.begin(), num.end()));
+
+	cout << ans << endl;
+
+	return 0;
+}
