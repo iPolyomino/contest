842f5f135f5ee1623f86b084d3d1324b6c82323b
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-02-26 00:02:19 +0900

    abc 188 abc

---
+++ atcoder/abc/188/b.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	int N;
+	cin >> N;
+	vector < int >A(N), B(N);
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
+	for (int i = 0; i < N; i++) {
+		cin >> B[i];
+	}
+
+	ll ans = 0;
+	for (int i = 0; i < N; i++) {
+		ans += A[i] * B[i];
+	}
+
+	if (ans == 0) {
+		cout << "Yes" << endl;
+	} else {
+		cout << "No" << endl;
+	}
+
+	return 0;
+}
---
+++ atcoder/abc/188/c.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector < ll > A(1 << N);
+	for (int i = 0; i < (1 << N); i++) {
+		cin >> A[i];
+	}
+
+	int half = 1 << (N - 1);
+	int max = max_element(A.begin(), A.end()) - A.begin();
+	auto start = max < half ? A.begin() + half : A.begin();
+
+	cout << max_element(start, start + half) - A.begin() + 1 << endl;
+
+	return 0;
+}
