0faef5f6e62c2fa91805234a60d10722eb54e857
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-02-26 10:39:16 +0900

    abc 190 ab

---
+++ atcoder/abc/190/b.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main() {
+  ll N, S, D;
+  cin >> N >> S >> D;
+  vector<ll> X(N), Y(N);
+  for (int i = 0; i < N; i++) {
+    cin >> X[i] >> Y[i];
+  }
+
+  for (int i = 0; i < N; i++) {
+    if (X[i] < S && Y[i] > D) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+
+  cout << "No" << endl;
+
+  return 0;
+}
