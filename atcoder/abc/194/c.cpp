3012317bc4fc7d9fdd0ad9f6f4a1038260f21c88
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-03-13 20:40:22 +0900

    abc 194 c

---
+++ atcoder/abc/194/c.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector < ll > A(N);
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
+
+	map < int, int >cnt;
+	for (int i = 0; i < N; i++) {
+		cnt[A[i]]++;
+	}
+
+	ll ans = 0;
+ for (auto & i:cnt) {
+ for (auto & j:cnt) {
+			ans += pow(i.first - j.first, 2) * i.second * j.second;
+		}
+	}
+
+	cout << ans / 2 << endl;
+
+	return 0;
+}
