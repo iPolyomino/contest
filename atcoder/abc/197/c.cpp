bec295134a48456aaa16ae9609879ad2eee23781
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-04-01 20:22:29 +0900

    abc 197 c

---
+++ atcoder/abc/197/c.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N;
+
+	cin >> N;
+
+	vector<int> A(N);
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
+
+	int ans = INT_MAX;
+	for (int bits = 0; bits < (1 << (N - 1)); bits++) {
+		int xored = 0;
+		int ored = 0;
+		for (int i = 0; i <= N; i++) {
+			if (i < N) ored |= A[i];
+			if (i == N || (bits >> i & 1)) xored ^= ored, ored = 0;
+		}
+		ans = min(ans, xored);
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
