54cbbd679c397e960026c3e8963133b4dc786be7
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-06-12 21:27:33 +0900

    abc 238 a

---
+++ atcoder/abc/238/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+	long long N;
+
+	cin >> N;
+
+	if (2 <= N && N <= 4 ) {
+		cout << "No" << endl;
+	} else {
+		cout << "Yes" << endl;
+	}
+	return 0;
+}
