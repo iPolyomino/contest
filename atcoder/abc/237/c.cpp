d07260cc68260df2710947055ec63f50a0f4fb1a
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2022-06-13 21:21:18 +0900

    abc 237 c

---
+++ atcoder/abc/237/c.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string S;
+
+	cin >> S;
+
+	int x = 0, y = 0;
+
+	for (size_t i = 0 ; i  < S.size(); i++) {
+		if (S[i] == 'a') {
+			x++;
+		} else {
+			break;
+		}
+	}
+
+	for (size_t i = S.size() - 1; i >= 0; i--) {
+		if (S[i] == 'a') {
+			y++;
+		} else {
+			break;
+		}
+	}
+
+	if (x == S.size()) {
+		cout << "Yes" << endl;
+		return 0;
+	}
+	if (x > y) {
+		cout << "No" << endl;
+		return 0;
+	}
+	for (int i = x; i < (S.size() - y); i++) {
+		if (S[i] != S[x + S.size() - y - i - 1]) {
+			cout << "No" << endl;
+			return 0;
+		}
+	}
+	cout << "Yes" << endl;
+
+	return 0;
+}
